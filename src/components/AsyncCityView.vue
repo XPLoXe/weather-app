0e0ad0e8156d621d15fd7ea600585e9540650989
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-30 12:18:08 +0200

    Fix: different Weather API used
    After a long time trying to figure out why the Open Weather MAp API was not responding, I simply switched to another one: WTTR, more simple and without API Key

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -8,57 +8,24 @@ import { useRoute } from 'vue-router'
 
 //https://openweathermap.org/api/one-call-api
 const route = useRoute()
-const APIKey = 'b1170961541492433b0b9317087c5639'
+//const APIKey = 'b1170961541492433b0b9317087c5639'
 
 const getWeatherData = async () => {
   try {
     console.log(route.query.lat)
     console.log(route.query.lng)
+    // const weatherData = await axios.get(
+    //   `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude=hourly,daily&appid=b1170961541492433b0b9317087c5639`
+    // )
     const weatherData = await axios.get(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${route.query.lat}&lon=${route.query.lng}&exclude={part}&appid=b1170961541492433b0b9317087c5639`
+      `https://wttr.in/${route.query.lat},${route.query.lng}?format=%C+%t`
     )
-
-    // cal current date & time
-    const localOffset = new Date().getTimezoneOffset() * 60000
-    const utc = weatherData.data.current.dt * 1000 + localOffset
-    weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone_offset
-
-    // cal hourly weather offset
-    weatherData.data.hourly.forEach((hour) => {
-      const utc = hour.dt * 1000 + localOffset
-      hour.currentTime = utc + 1000 * weatherData.data.timezone_offset
-    })
 
     return weatherData
   } catch (error) {
     console.log(error)
   }
 }
-
-// const getWeatherAPI = async () => {
-//   try {
-//     console.log(route.query.lat)
-//     console.log(route.query.lng)
-//     const weatherData = await axios.get(
-//       `http://api.weatherapi.com/v1/current.json?key=b25b84160ae541ecb4a80024233008?q=Paris`
-//     )
-
-//     // cal current date & time
-//     const localOffset = new Date().getTimezoneOffset() * 60000
-//     const utc = weatherData.data.current.dt * 1000 + localOffset
-//     weatherData.data.currentTime = utc + 1000 * weatherData.data.timezone_offset
-
-//     // cal hourly weather offset
-//     weatherData.data.hourly.forEach((hour) => {
-//       const utc = hour.dt * 1000 + localOffset
-//       hour.currentTime = utc + 1000 * weatherData.data.timezone_offset
-//     })
-
-//     return weatherData
-//   } catch (error) {
-//     console.log(error)
-//   }
-// }
 
 const weatherData = await getWeatherData()
 console.log(weatherData)
--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <AsyncCityView></AsyncCityView>
+    <Suspense>
+      <AsyncCityView></AsyncCityView>
+      <template #fallback>
+        <p>Loading...</p>
+      </template>
+    </Suspense>
   </div>
 </template>
 
