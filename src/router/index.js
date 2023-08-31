5710064c9f810dfc57ca507082fe4df8a50efcb5
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-31 11:53:48 +0200

    update: route params

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/weather/:country/:city',
+      path: '/weather/:state/:city',
       name: 'cityView',
       component: CityView
     }
