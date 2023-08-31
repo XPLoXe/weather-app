75452d6b5d00423babf4c2d9d8fda6b5448e6c64
Alejandro Bocchicchio <aleb2800@gmail.com>
2023-08-31 12:40:43 +0200

    fix: states will be displayed with spaces

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -51,7 +51,7 @@ const previewCity = (searchResult) => {
   const [city, state] = searchResult.place_name.split(',')
   router.push({
     name: 'cityView',
-    params: { state: state.replaceAll(' ', ''), city: city },
+    params: { state: state, city: city },
     query: {
       lat: searchResult.geometry.coordinates[1],
       lng: searchResult.geometry.coordinates[0],
